518ef3f7ab415e692639145d0adb212c903401eb
Peter Phillips <peter@greenthistle.co.uk>
2020-03-24 11:44:57 +0000

    DT-618: Initial stab at token verification service

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # token-verification-api
+
+[![CircleCI](https://circleci.com/gh/ministryofjustice/token-verification-api/tree/master.svg?style=svg)](https://circleci.com/gh/ministryofjustice/token-verification-api)
+[![Known Vulnerabilities](https://snyk.io/test/github/ministryofjustice/token-verification-api/badge.svg)](https://snyk.io/test/github/ministryofjustice/token-verification-api)
+
+Self-contained fat-jar micro-service to control verification of access and refresh tokens
+ 
+### Building
+
+```bash
+./gradlew build
+```
+
+### Running
+
+```bash
+./gradlew bootRun
+```
+
+#### Health
+
+- `/health/ping`: will respond with status `UP` to all requests.  This should be used by dependent systems to check connectivity to token-verification-api,
+rather than calling the `/health` endpoint.
+- `/health`: provides information about the application health and its dependencies.  This should only be used
+by token-verification-api health monitoring (e.g. pager duty) and not other systems who wish to find out the state of token-verification-api.
+- `/info`: provides information about the version of deployed application.
+
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/TokenVerificationExceptionHandler.kt
@@ -0,0 +1,38 @@
+package uk.gov.justice.digital.hmpps.tokenverification
+
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import javax.validation.ValidationException
+
+
+@RestControllerAdvice
+class TokenVerificationExceptionHandler {
+  @ExceptionHandler(ValidationException::class)
+  fun handleValidationException(e: Exception): ResponseEntity<ErrorResponse> {
+    log.info("Validation exception: {}", e.message)
+    return ResponseEntity
+        .status(HttpStatus.BAD_REQUEST)
+        .body(ErrorResponse(status = HttpStatus.BAD_REQUEST, developerMessage = e.message))
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(TokenVerificationExceptionHandler::class.java)
+  }
+}
+
+
+data class ErrorResponse(val status: Int,
+                         val errorCode: Int? = null,
+                         val userMessage: String? = null,
+                         val developerMessage: String? = null,
+                         val moreInfo: String? = null) {
+  constructor(status: HttpStatus,
+              errorCode: Int? = null,
+              userMessage: String? = null,
+              developerMessage: String? = null,
+              moreInfo: String? = null)
+      : this(status.value(), errorCode, userMessage, developerMessage, moreInfo)
+}
