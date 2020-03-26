48a89ce49895d27d4319ae5fe9dd6329cbd9a28a
Peter Phillips <peter@greenthistle.co.uk>
2020-03-26 07:38:12 +0000

    DT-618: First stab at token verification repo

--- src/test/kotlin/uk/gov/justice/digital/hmpps/tokenverification/resource/IntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/tokenverification/resource/IntegrationTest.kt
@@ -1,18 +1,15 @@
 package uk.gov.justice.digital.hmpps.tokenverification.resource
 
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.BeforeAll
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.reactive.server.WebTestClient
-import redis.embedded.RedisServer
 
 
 @Suppress("SpringJavaInjectionPointsAutowiringInspection")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-abstract class IntegrationTest {
+abstract class IntegrationTest : RedisTest() {
   @Suppress("unused")
   @Autowired
   lateinit var webTestClient: WebTestClient
@@ -20,22 +17,5 @@ abstract class IntegrationTest {
   init {
     // Resolves an issue where Wiremock keeps previous sockets open from other tests causing connection resets
     System.setProperty("http.keepAlive", "false")
-  }
-
-  companion object {
-    @JvmField
-    val redisServer: RedisServer = RedisServer(6379)
-
-    @BeforeAll
-    @JvmStatic
-    fun startRedis() {
-      redisServer.start()
-    }
-
-    @AfterAll
-    @JvmStatic
-    fun stopRedis() {
-      redisServer.stop()
-    }
   }
 }
