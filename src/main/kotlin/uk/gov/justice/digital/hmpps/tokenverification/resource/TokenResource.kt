6ddb39bc3de3966e18184257f832be3523c1200a
Peter Phillips <peter@greenthistle.co.uk>
2020-03-31 11:37:41 +0100

    DT-618: Add in swagger documentation

--- src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/resource/TokenResource.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/resource/TokenResource.kt
@@ -2,6 +2,10 @@ package uk.gov.justice.digital.hmpps.tokenverification.resource
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.ApiResponses
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import springfox.documentation.annotations.ApiIgnore
+import uk.gov.justice.digital.hmpps.tokenverification.config.ErrorResponse
 import uk.gov.justice.digital.hmpps.tokenverification.service.TokenService
 
 @RestController
@@ -19,7 +24,14 @@ import uk.gov.justice.digital.hmpps.tokenverification.service.TokenService
 @RequestMapping("/token", produces = [MediaType.APPLICATION_JSON_VALUE])
 class TokenResource(private val tokenService: TokenService) {
   @PostMapping("verify")
-  fun verifyToken(@RequestBody jwt: String): TokenDto = tokenService.verifyToken(jwt)
+
+  @ApiOperation(value = "Verify that a JWT is still valid.",
+      notes = """A successful request to this API will return a <code>HTTP 200 - Success</code>, but this doesn't
+                 indicate that the JWT is valid.  You need to check the boolean <code>active</code> flag which is
+                 returned in the payload body.""")
+  @ApiResponses(value = [ApiResponse(code = 400, message = "Bad request.  The JWT is invalid or has expired.", response = ErrorResponse::class, responseContainer = "List")])
+  fun verifyToken(@RequestBody @ApiParam(value = "JWT to check")
+                  jwt: String): TokenDto = tokenService.verifyToken(jwt)
 
   @ApiIgnore
   @PreAuthorize("hasRole('AUTH_TOKEN_VERIFICATION')")
