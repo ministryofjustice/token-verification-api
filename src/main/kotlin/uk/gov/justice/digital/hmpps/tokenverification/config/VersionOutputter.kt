09fdb646266829c4a208a8b7c2233cbedb8c68dd
Peter Phillips <peter@greenthistle.co.uk>
2020-03-31 17:28:35 +0100

    DT-618: Add version to application insights too

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/config/VersionOutputter.kt
@@ -0,0 +1,32 @@
+package uk.gov.justice.digital.hmpps.tokenverification.config
+
+import com.microsoft.applicationinsights.extensibility.ContextInitializer
+import com.microsoft.applicationinsights.telemetry.TelemetryContext
+import org.slf4j.LoggerFactory
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.boot.info.BuildProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.event.EventListener
+
+@Configuration
+class VersionOutputter(buildProperties: BuildProperties) {
+  private val version = buildProperties.version
+
+  @EventListener(ApplicationReadyEvent::class)
+  fun logVersionOnStartup() {
+    log.info("Version {} started", version)
+  }
+
+  @Bean
+  fun versionContextInitializer() = ContextInitializer {
+    @Suppress("unused")
+    fun initialize(context: TelemetryContext) {
+      context.component.setVersion(version)
+    }
+  }
+
+  companion object {
+    private val log = LoggerFactory.getLogger(VersionOutputter::class.java)
+  }
+}
