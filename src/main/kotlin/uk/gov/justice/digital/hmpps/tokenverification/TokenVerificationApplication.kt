b54e2bedc98ef7745d5a35a7de58f893ad1303de
petergphillips <peter@greenthistle.co.uk>
2020-03-31 22:09:59 +0100

    DT-618: Add version to application insights too (#12)
    * DT-618: Add version to application insights too
    
    * DT-618: Actually implement the method

--- src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/TokenVerificationApplication.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/tokenverification/TokenVerificationApplication.kt
@@ -1,12 +1,8 @@
 package uk.gov.justice.digital.hmpps.tokenverification
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.event.ApplicationReadyEvent
-import org.springframework.boot.info.BuildProperties
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.event.EventListener
 import org.springframework.data.redis.core.RedisKeyValueAdapter.EnableKeyspaceEvents
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
@@ -25,15 +21,3 @@ fun main(args: Array<String>) {
     enableKeyspaceEvents = EnableKeyspaceEvents.ON_STARTUP,
     keyspaceNotificationsConfigParameter = "\${application.keyspace-notifications:}")
 class AppConfig
-
-@Configuration
-class VersionLogger(val buildProperties: BuildProperties) {
-  @EventListener(ApplicationReadyEvent::class)
-  fun logVersionOnStartup() {
-    log.info("Version {} started", buildProperties.version)
-  }
-
-  companion object {
-    private val log = LoggerFactory.getLogger(VersionLogger::class.java)
-  }
-}
