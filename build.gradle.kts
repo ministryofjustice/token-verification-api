31477c1964c4b96ec073427619a87293ab872d01
Peter Phillips <peter@greenthistle.co.uk>
2020-05-11 12:06:34 +0100

    DT-826: Upgrade spring boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.1.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "0.3.0"
   kotlin("plugin.spring") version "1.3.72"
   kotlin("plugin.jpa") version "1.3.72"
 }
 
+extra["spring-security.version"] = "5.3.2.RELEASE" // Updated since spring-boot-starter-oauth2-resource-server-2.2.7.RELEASE only pulls in 5.2.4.RELEASE (still affected by CVE-2018-1258 though)
 
-extra["spring-security.version"] = "5.3.1.RELEASE" // Updated since spring-boot-starter-oauth2-resource-server-2.2.5.RELEASE only pulls in 5.2.2.RELEASE (still affected by CVE-2018-1258 though)
+configurations {
+  testImplementation { exclude(group = "org.junit.vintage") }
+}
 
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
