391d1222df73c1b039e8745206f6d1fe60ec589a
Karen Miller <75248289+karenmillermoj@users.noreply.github.com>
2022-11-07 11:15:35 +0000

    HAAR-974: Remove springfox (#138)
    * HAAR-974: Remove springfox
    
    * HAAR-974: Ensure redis running for openapi docs test
    
    * HAAR-974: remove unnecessary mavenCentral

---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/tokenverification/resource/OpenApiDocsTest.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.tokenverification.resource
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.http.MediaType.APPLICATION_JSON
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter.ISO_DATE
+
+@ExtendWith(RedisExtension::class)
+class OpenApiDocsTest : IntegrationTest() {
+
+  @Test
+  fun `open api docs are available`() {
+    webTestClient.get()
+      .uri("/webjars/swagger-ui/index.html?configUrl=/v3/api-docs")
+      .accept(APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+  }
+
+  @Test
+  fun `open api docs redirect to correct page`() {
+    webTestClient.get()
+      .uri("/swagger-ui.html")
+      .accept(APPLICATION_JSON)
+      .exchange()
+      .expectStatus().is3xxRedirection
+      .expectHeader().value("Location") { it.contains("/swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config") }
+  }
+
+  @Test
+  fun `the swagger json is valid`() {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("messages").doesNotExist()
+  }
+
+  @Test
+  fun `the swagger json contains the version number`() {
+    webTestClient.get()
+      .uri("/v3/api-docs")
+      .accept(APPLICATION_JSON)
+      .exchange()
+      .expectStatus().isOk
+      .expectBody().jsonPath("info.version").isEqualTo(ISO_DATE.format(LocalDate.now()))
+  }
+}
