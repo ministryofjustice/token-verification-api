f0b6c06dd074ef9b2f4233af459f2a779683476f
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2023-04-26 18:13:35 +0100

    HAAR-1395: spring-boot-upgrade from 4.8.6-beta-3 to 5.1.4-beta-3

--- src/test/kotlin/uk/gov/justice/digital/hmpps/tokenverification/service/JwtAuthHelper.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/tokenverification/service/JwtAuthHelper.kt
@@ -41,7 +41,7 @@ class JwtAuthHelper {
       .setSubject(subject)
       .addClaims(claims)
       .setExpiration(Date(System.currentTimeMillis() + expiryTime.toMillis()))
-      .signWith(keyPair.private, SignatureAlgorithm.RS256)
+      .signWith(SignatureAlgorithm.RS256, keyPair.private)
       .compact()
   }
 }
