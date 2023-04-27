5f45553cb7143b27c8792ae3224e19578f47ad24
R Kumar <ravindra.kumar@digital.justice.gov.uk>
2023-04-27 09:25:44 +0100

    HAAR-1395: spring-boot-upgrade from 4.8.6-beta-3 to 5.1.4-beta-3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,22 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.8.6-beta-3"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4-beta-3"
   kotlin("plugin.spring") version "1.8.21"
   kotlin("plugin.jpa") version "1.8.21"
 }
 
 configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
+  testImplementation { exclude(module = "slf4j-simple") }
 }
 
 dependencyCheck {
   suppressionFiles.add("token-verification-suppressions.xml")
 }
 
+repositories {
+  maven { url = uri("https://repo.spring.io/milestone") }
+  mavenCentral()
+}
 dependencies {
   annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 
@@ -19,16 +24,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.7.0")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.7.0")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.7.0")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.2")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
   implementation("com.google.guava:guava:31.1-jre")
 
   testImplementation("org.springframework.boot:spring-boot-starter-webflux")
   testImplementation("it.ozimov:embedded-redis:0.7.3")
-  testImplementation("io.jsonwebtoken:jjwt:0.9.1")
+  testImplementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+  testImplementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
   testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
 }
 
