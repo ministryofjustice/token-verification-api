0fafef55ea0dc9489d18c650162f5b96e6c19ff2
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-09-12 13:23:35 +0100

    HAAR-1805: ⬆️ update to java 20 (#177)
    * HAAR-1805: ⬆️ update to java 20
    
    * HAAR-1805: ⬆️ update circle config.yml to java 20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.0"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.4.1"
   kotlin("plugin.spring") version "1.9.10"
   kotlin("plugin.jpa") version "1.9.10"
 }
@@ -34,13 +34,13 @@ dependencies {
 }
 
 java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(19))
+  toolchain.languageVersion.set(JavaLanguageVersion.of(20))
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "19"
+      jvmTarget = "20"
     }
   }
 }
