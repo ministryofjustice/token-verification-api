2c96176ded8ab19d7ef65f48a0d2863e9a508b6a
SimonMitchellMOJ <51327941+SimonMitchellMOJ@users.noreply.github.com>
2023-12-11 15:23:57 +0000

    HAAR-2155: ⬆️ update to java 21 (#185)
    * HAAR-2155: ⬆️ update to java 21
    
    * HAAR-2155: ⬆️ update to java 21
    
    * HAAR-2155: ⬆️ update to java gradle spring boot plugin 5.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
 
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.7.0"
-  kotlin("plugin.spring") version "1.9.10"
-  kotlin("plugin.jpa") version "1.9.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.8.0"
+  kotlin("plugin.spring") version "1.9.20"
+  kotlin("plugin.jpa") version "1.9.20"
 }
 
 configurations {
@@ -33,14 +33,14 @@ dependencies {
   testImplementation("org.mockito.kotlin:mockito-kotlin:5.1.0")
 }
 
-java {
-  toolchain.languageVersion.set(JavaLanguageVersion.of(20))
+kotlin {
+  jvmToolchain(21)
 }
 
 tasks {
   withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-      jvmTarget = "20"
+      jvmTarget = "21"
     }
   }
 }
