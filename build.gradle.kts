f32ca7483fa053f442c6ff24b0eaf4c869ca247b
Mohamad Agil <mohamad.agil@digital.justice.gov.uk>
2024-10-28 14:15:23 +0000

    HAAR-3187: ⬆️ Upgrading to Spring Boot 3.3.5 to resolve CVE

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.6"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
   kotlin("plugin.spring") version "2.0.20"
   kotlin("plugin.jpa") version "2.0.20"
 }
